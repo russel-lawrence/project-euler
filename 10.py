2ca9ba108b02916baadaf2fb12c8ad18515bbbd4
russel-lawrence <russel007lawrence@gmail.com>
2017-04-29 15:09:00 +0530

    trial

--- 10.py
+++ 10.py
@@ -2,11 +2,11 @@ import math
 s, num = 2, 3
 while num <= 2000000:
     t = True
-    for i in range(3,int(math.sqrt(num))+1,2):
-        t = t and num%i != 0
+    for i in range (3, int (math.sqrt(num)) + 1, 2):
+        t = t and num % i != 0
     if t == True:
         s = s+num
     num = num + 2
-print(s)
+print (s)
 
 
