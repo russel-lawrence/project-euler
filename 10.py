9c3c12db3e11c9e2fffa8b1023bfd72f45d0a27b
russel-lawrence <russel007lawrence@gmail.com>
2017-04-29 15:04:29 +0530

    made some minor changes that do not affect the code

--- 10.py
+++ 10.py
@@ -1,11 +1,11 @@
 import math
-s,num=2,3
-while num<=2000000:
-    t=True
+s, num = 2, 3
+while num <= 2000000:
+    t = True
     for i in range(3,int(math.sqrt(num))+1,2):
-        t=t and num%i!=0
-    if t==True:
-        s=s+num
+        t = t and num%i != 0
+    if t == True:
+        s = s+num
     num = num + 2
 print(s)
 
